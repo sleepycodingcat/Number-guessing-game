fd32bfb68acdb78ac051ad165c4c04487c24ce5c
Rachell Aristo <123459774+sleepycodingcat@users.noreply.github.com>
2024-07-19 15:42:31 +0200

    Add files via upload

---
+++ guessGame.js
@@ -0,0 +1,13 @@
+let lives = 5;
+let max = 25;
+let min = 0;
+
+const submit = document.getElementById("guessSubmit");
+const hint = document.getElementById("hint");
+const guess = document.getElementById("guess");
+
+
+submit.onclick = function(){
+    console.log(guess);
+
+}
